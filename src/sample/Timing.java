c7cebaa47b4d41b36f5c86b669574d8f6a7a595a
SUCHIAWEI <32055784+SUCHIAWEI@users.noreply.github.com>
2018-11-01 01:13:58 +0800

    hw

---
+++ src/sample/Timing.java
@@ -0,0 +1,35 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.awt.event.ActionEvent;
+import java.util.Timer;
+
+public class Timing {
+    Image img0 = new Image("0.GIF");
+    Image img1 = new Image("1.GIF");
+    Image img2 = new Image("2.GIF");
+    Image img3 = new Image("3.GIF");
+    Image img4 = new Image("4.GIF");
+    Image img5 = new Image("5.GIF");
+    Image img6 = new Image("6.GIF");
+    Image img7 = new Image("7.GIF");
+    Image img8 = new Image("8.GIF");
+    Image img9 = new Image("9.GIF");
+
+    @FXML
+    ImageView imgv1 , imgv2 , imgv3 , imgv4 ;
+    @FXML
+    Button run;
+
+    int min =
+
+    public void run(){
+        t.start();
+
+    }
+
+}
